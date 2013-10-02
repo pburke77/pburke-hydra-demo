d4f5204ee849f16c2a5ab3aed8d11b2f905a1775
Patrick Burke <burkepk@uc.edu>
2013-10-02 09:24:22 -0400

    Catching up

---
+++ app/models/page.rb
@@ -0,0 +1,11 @@
+class Page < ActiveFedora::Base
+  has_metadata 'descMetadata', type: PageMetadata
+
+  belongs_to :book, :property=> :is_part_of
+
+  delegate :number, to: 'descMetadata', unique: :true
+  delegate :text, to: 'descMetadata', unique: :true
+  
+  has_file_datastream "pageContent"
+  
+end
